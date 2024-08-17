a2226529a4f9121d78535aab18f03b8b60119955
Ali <aliwebdevf@gmail.com>
2024-08-17 11:52:52 +0330

    build

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,3 +1,102 @@
-const a: string = "hi";
+import Splide from "@splidejs/splide";
+import "@splidejs/splide/css/core";
 
-console.log(a);
+new Splide(".splide-comments", {
+  perPage: 3,
+  gap: "1rem",
+  perMove: 1,
+  direction: "rtl",
+  pagination: false,
+
+  breakpoints: {
+    740: {
+      perPage: 1,
+      perMove: 1,
+      width: "100%",
+      fixedWidth: "100%",
+    },
+  },
+}).mount();
+
+new Splide(".splide-articles", {
+  perPage: 3,
+  gap: "1rem",
+  perMove: 1,
+  direction: "rtl",
+  pagination: false,
+
+  breakpoints: {
+    540: {
+      perPage: 1,
+      perMove: 1,
+      fixedWidth: "100%",
+    },
+    800: {
+      perPage: 2,
+    },
+  },
+}).mount();
+
+new Splide(".splide-supports", {
+  perPage: 6,
+  gap: "3rem",
+  perMove: 2,
+  direction: "rtl",
+  arrows: false,
+  pagination: false,
+
+  breakpoints: {
+    600: {
+      perPage: 4,
+      perMove: 1,
+    },
+  },
+}).mount();
+
+new Splide(".splide-why", {
+  perPage: 4,
+  gap: "1rem",
+  perMove: 1,
+  direction: "rtl",
+  padding: "1rem",
+  pagination: false,
+  breakpoints: {
+    540: {
+      perPage: 1,
+      perMove: 1,
+    },
+    800: {
+      perPage: 2,
+    },
+  },
+}).mount();
+
+let question = document.querySelectorAll(".question");
+
+question.forEach((question) => {
+  question.addEventListener("click", () => {
+    const active = document.querySelector(".question.active") as HTMLElement;
+    if (active && active !== question) {
+      active.classList.toggle("active");
+      (active.nextElementSibling as HTMLElement).style.maxHeight = " 0";
+    }
+    question.classList.toggle("active");
+    const answer = question.nextElementSibling as HTMLElement;
+    if (question.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = "0";
+    }
+  });
+});
+
+const toggleNav = document.querySelector(".toggle-nav-menu");
+const navMenu = document.querySelector(".nav-menu")! as HTMLElement;
+
+toggleNav?.addEventListener("click", () => {
+  if (navMenu.style.gridTemplateRows == "1fr") {
+    navMenu.style.gridTemplateRows = "0fr";
+  } else {
+    navMenu.style.gridTemplateRows = "1fr";
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,24 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "460px",
+      "mid-sm": "600px",
+      md: "917px",
+    },
     extend: {
       backgroundImage: {
         "request-form": "url('./assets/images/request-form-bg.png')",
+        "request-demo": "url('./assets/images/request-demo-bg.png')",
       },
       colors: {
         "orange-400": "#F9B233",
+        "gray-300": "#B2B2B2",
+        "green-600": "#2FAC66",
+        "gray-800": "#515151", // Change later to /50 shade of gray-800 if required
+        "green-350": "#EAF7F0",
+        "gray-400": "#DFDFDF",
+        circle: "rgba(29,122,85,30%)",
       },
     },
   },

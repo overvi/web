425dd949f29c13c7ac71c9a944e029365eb8e14a
Ali <aliwebdevf@gmail.com>
2024-08-23 08:55:41 +0330

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}", "./src/ts/*.ts"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     screens: {
       sm: "460px",
